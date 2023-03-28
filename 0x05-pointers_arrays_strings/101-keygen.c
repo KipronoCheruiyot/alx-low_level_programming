7d41950b96639d46210b33737ec8d46012fa7450
KipronoCheruiyot <cheruiyotbrian976@gmail.com>
2023-03-28 21:58:48 +0300

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 11
+
+int main(void) {
+    char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % (sizeof charset - 1)];
+    }
+
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("Generated password: %s\n", password);
+
+    return 0;
+}
